14f6b6a7df3631820a7dd1dd037aa2c64bfcc5ea
Pedro Mariutti <pmariutti@gmail.com>
2025-11-08 17:08:03 -0300

    Commit 1: Setup inicial do projeto (Vite + React + TS)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Diz ao Tailwind para ler todos os seus ficheiros de front-end
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
